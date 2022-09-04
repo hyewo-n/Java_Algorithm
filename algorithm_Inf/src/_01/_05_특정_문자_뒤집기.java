b1609978ad4d07c4244c7cf50f1d9ccf03ac9557
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-04 23:59:13 +0900

    220904

--- algorithm_Inf/src/_01/_05_특정_문자_뒤집기.java
+++ algorithm_Inf/src/_01/_05_특정_문자_뒤집기.java
@@ -1,7 +1,34 @@
 package _01;
+import java.util.*;
 
 public class _05_특정_문자_뒤집기 {
-
-	Build path entry is missing: org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-13
-
+	public static void main(String[] args) {
+	
+		String answer = "";
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char[] s = str.toCharArray();
+		int lt =0;
+		int rt = str.length()-1;
+		
+		while(lt<rt) {
+			if(!Character.isAlphabetic(s[lt]))
+				lt++;
+			else if(!Character.isAlphabetic(s[rt]))
+				rt--;
+			else {
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
+			}
+		}
+		answer = String.valueOf(s);
+		System.out.println(answer);
+		
+		
+		
+	}
 }
