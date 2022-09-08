c6e0a1f8cd66b6d4fe7a1d5a03d6d427fc8f1541
gpdnjs <heart0412@naver.com>
2022-09-08 21:42:16 +0900

    .

---
+++ algorithm_Inf/src/_04/_01_학급_회장.java
@@ -0,0 +1,22 @@
+package _04;
+import java.util.*;
+
+public class _01_학급_회장 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String str = sc.next();
+		
+		for(int i=0; i<str.length(); i++) {
+			switch(str.charAt(i)) {
+			case 'A':
+				
+			}
+		}
+		
+	}
+
+}
