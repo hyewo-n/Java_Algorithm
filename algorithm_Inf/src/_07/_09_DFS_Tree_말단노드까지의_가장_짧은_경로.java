a2ed203c643744d91eccf8cc36ddfc29f37c45f7
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-12 23:15:23 +0900

    .

--- algorithm_Inf/src/_07/_09_DFS_Tree_말단노드까지의_가장_짧은_경로.java
+++ algorithm_Inf/src/_07/_09_DFS_Tree_말단노드까지의_가장_짧은_경로.java
@@ -1,9 +1,38 @@
 package _07;
+import java.util.*;
+
+/*
+class Node{
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		data = val;
+		lt=rt=null;
+	}
+} */
 
 public class _09_DFS_Tree_말단노드까지의_가장_짧은_경로 {
 
+	Node root;
+	public static int DFS(int L, Node root) {   // 여기 root는 노드를 가르키는 것이지 진짜 root로 고정된 것이 아님
+		if(root.lt==null && root.rt==null){
+			return L;
+		}else{
+			return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		_09_DFS_Tree_말단노드까지의_가장_짧은_경로 tree = new _09_DFS_Tree_말단노드까지의_가장_짧은_경로();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		System.out.println(tree.DFS(0, tree.root));
+		
+
 
 	}
 
--- algorithm_Inf/src/_07/_10_BFS_Tree_말단노드까지의_가장_짧은_경로.java
+++ algorithm_Inf/src/_07/_10_BFS_Tree_말단노드까지의_가장_짧은_경로.java
@@ -1,9 +1,48 @@
 package _07;
+import java.util.*;
+
+/*
+class Node{
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		data = val;
+		lt=rt=null;
+	}
+} */
 
 public class _10_BFS_Tree_말단노드까지의_가장_짧은_경로 {
 
+	Node root;
+	public static int BFS(Node root) {
+		Queue<Node> Q = new LinkedList<>(); 
+		Q.offer(root);
+		int L = 0;
+		while(!Q.isEmpty()) {
+			int len = Q.size();
+			for(int i=0; i<len; i++) {
+				 Node cur = Q.poll();
+				 if(cur.lt==null && cur.rt==null)
+					 return L;
+				 if(cur.lt != null)  // 자식이 있는 것
+					 Q.offer(cur.lt);
+				 if(cur.rt != null)  // 자식이 있는 것
+					 Q.offer(cur.rt);
+			}
+			L++;
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		_10_BFS_Tree_말단노드까지의_가장_짧은_경로 tree = new _10_BFS_Tree_말단노드까지의_가장_짧은_경로();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		System.out.println(tree.BFS(tree.root));
 
 	}
 
