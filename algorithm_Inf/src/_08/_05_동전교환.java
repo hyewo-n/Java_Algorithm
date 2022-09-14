13235038e8ced866282d7881da38caea013b15e1
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-15 04:25:57 +0900

    .

---
+++ algorithm_Inf/src/_08/_05_동전교환.java
@@ -0,0 +1,40 @@
+package _08;
+import java.util.*;
+
+public class _05_동전교환 {
+
+	static int n, m;
+	static int answer = Integer.MAX_VALUE;
+	public static void DFS(int L, int sum, Integer[] arr) {
+		if(sum>m)
+			return;
+		if(L >=answer)
+			return;
+		if(sum==m) {
+			answer = Math.min(answer, L);
+		}else {
+			for(int i=0; i<n; i++) {
+				DFS(L+1, sum+arr[i], arr); 
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		Integer arr[] = new Integer[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+		m = sc.nextInt(); 
+		// 입력 완료
+		
+		DFS(0, 0, arr);
+		System.out.println(answer);
+
+	}
+
+}
---
+++ algorithm_Inf/src/_08/_06_순열_구하기.java
@@ -0,0 +1,20 @@
+package _08;
+import java.util.*;
+
+public class _06_순열_구하기 {
+
+	public static void DFS() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int arr[] = new int[n];
+		DFS();
+
+	}
+
+}
