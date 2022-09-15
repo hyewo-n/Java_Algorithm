d7028d709ec7d6e62fe3344b3ab2067be4da7545
gpdnjs <heart0412@naver.com>
2022-09-15 21:49:11 +0900

    .

--- algorithm_Inf/src/_08/_06_순열_구하기.java
+++ algorithm_Inf/src/_08/_06_순열_구하기.java
@@ -3,17 +3,39 @@ import java.util.*;
 
 public class _06_순열_구하기 {
 
-	public static void DFS() {
-		
+	static int[] pm, ch, arr;
+	static int n, m;
+	
+	public static void DFS(int L) {
+		if(L==m) {
+			for(int x : pm)
+				System.out.print(x + " ");
+			System.out.println();
+		}else{
+			for(int i=0; i<n; i++) {
+				if(ch[i]==0) {
+					ch[i]=1;
+					pm[L] = arr[i];
+					DFS(L+1);
+					ch[i]=0;     // Back 하는 지점
+ 				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int arr[] = new int[n];
-		DFS();
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		ch = new int[n];
+		pm = new int[m];
+		
+		DFS(0);
 
 	}
 
---
+++ algorithm_Inf/src/_08/_07_조합의_경우의_수_메모이제이션.java
@@ -0,0 +1,20 @@
+package _08;
+import java.util.*;
+
+public class _07_조합의_경우의_수_메모이제이션 {
+
+	static int n, r;
+	public static void DFS() {
+		
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		r = sc.nextInt();
+		DFS();
+	}
+
+}
