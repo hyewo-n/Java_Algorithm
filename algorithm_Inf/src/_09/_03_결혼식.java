f28f76d3df1f36a875bd7c66b9612a66fb39c131
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-18 00:37:59 +0900

    .

---
+++ algorithm_Inf/src/_09/_03_결혼식.java
@@ -0,0 +1,49 @@
+package _09;
+import java.util.*;
+
+class Time2 implements Comparable<Time2>{
+	public int time;
+	public char state;
+	Time2(int time, char state) {
+		this.time = time;
+		this.state = state;
+	}
+	@Override
+	public int compareTo(Time2 ob) {
+		if(this.time == ob.time)
+			return this.state - ob.state;
+		else
+			return this.time - ob.time;
+	}
+}
+
+public class _03_결혼식 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		ArrayList<Time2> arr = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			int sT = sc.nextInt();
+			int eT = sc.nextInt();
+			arr.add(new Time2(sT, 's'));
+			arr.add(new Time2(eT, 'e'));
+		}  // 입력완료
+		
+		int answer = Integer.MIN_VALUE;
+		
+		Collections.sort(arr);
+		int cnt = 0;  // 현재 몇 명이 존재하는가
+		for(Time2 ob : arr) {
+			if(ob.state == 's')
+				cnt++;
+			else
+				cnt--;
+			answer = Math.max(answer, cnt);
+		}
+		System.out.println(answer);
+	}
+
+}
---
+++ algorithm_Inf/src/_09/_04_최대_수입_스케줄.java
@@ -0,0 +1,58 @@
+package _09;
+import java.util.*;
+
+class Lecture implements Comparable<Lecture> {
+	public int money;
+	public int time;
+	Lecture(int money, int time) {
+		this.money = money;
+		this.time = time;
+	}
+	@Override
+	public int compareTo(Lecture ob) {
+		// 내림차순 정렬
+		return ob.time - this.time;
+	}
+}
+
+public class _04_최대_수입_스케줄 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		ArrayList<Lecture> arr = new ArrayList<>();
+		int max = Integer.MIN_VALUE;
+		
+		for(int i=0; i<n; i++) {
+			int m = sc.nextInt();
+			int d = sc.nextInt();
+			arr.add(new Lecture(m, d));
+						
+			// 날짜 중에 가장 큰 값
+			if(d > max)
+				max = d;
+		}
+			int answer = 0;
+			
+			// 큰 값 우선순위로 하는 큐가 됨
+			PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+			
+			Collections.sort(arr);
+			
+			int j =0;
+			for(int k = max; k>=1; k--) {
+				for( ; j<n; j++) {
+					// 큐에 넣음
+					if(arr.get(j).time < k)
+						break;
+					pQ.offer(arr.get(j).money);
+				}
+				if(!pQ.isEmpty())
+					answer += pQ.poll();
+			}
+			System.out.println(answer);
+	}
+
+}
