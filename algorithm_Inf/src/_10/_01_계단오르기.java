e851d911588da699086de314f9b6a63d12c04961
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2022-09-20 16:33:47 +0900

    .

---
+++ algorithm_Inf/src/_10/_01_계단오르기.java
@@ -0,0 +1,22 @@
+package _10;
+import java.util.*;
+
+public class _01_계단오르기 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] dy = new int [n+1];
+		dy[1] = 1;
+		dy[2] = 2;
+		for(int i = 3; i<=n; i++) {
+			dy[i] = dy[i-2] + dy[i-1];
+		}
+		System.out.println(dy[n]);
+ 
+	}
+
+}
